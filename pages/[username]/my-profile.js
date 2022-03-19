959cb8cd3dfba1f718a2e36a97badb223b315c75
Chris Josephs <chrismjosephs@gmail.com>
2022-03-19 11:53:02 -0700

    Make skeleton for profile view page

--- pages/[username]/my-profile.js
+++ pages/[username]/my-profile.js
@@ -1,3 +1,23 @@
 // Profile View
+import { Image } from 'next/image';
 
-export default function MyProfile(props) {}
+// import components
+
+// information needed
+// name, city, state, user type, profile photo
+// Can use constant profile picture if user doesnt upload one
+// if Mentor, classes teaching, mentor rating
+
+export default function MyProfile(props) {
+  return (
+    <div>
+      <Image src={props.profilePhoto} alt={props.username} />
+      {props.userType === 'mentor' && <div>Mentor: Placehoder for classes I teach component</div>}
+      <h1>{props.firstName} {props.lastName}</h1>
+      <h2>{props.city} {props.state}</h2>
+      <p>Placeholder information</p>
+      {props.userType === 'mentor' && <div>Mentor: Placehoder for rating component</div>}
+      {props.userType === 'student' && <div>Student: Placehoder for write a review component</div>}
+    </div>
+  )
+}
