369b45d8a11a837509c7800b06083136e8fbf56b
Chris Josephs <chrismjosephs@gmail.com>
2022-03-19 09:37:45 -0700

    Add landing page title tag

--- pages/index.js
+++ pages/index.js
@@ -2,4 +2,8 @@
 
 import Head from 'next/head'
 
-export default function LandingPage() {}
+export default function LandingPage() {
+  return (
+    <h1>Landing Page</h1>
+  )
+}
--- utils/apiCalls.js
+++ utils/apiCalls.js
@@ -1,4 +1,5 @@
 // Standardized API calls for front end
+// Try to use nextjs fetch() method instead of axios
 // Error handling:
 //    Take care of NETWORK related errors in this file ie bad requests
 //    Response data errors should be handled in usaged area ie code expects an object but recieving an array
