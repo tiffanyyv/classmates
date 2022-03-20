ab919cffcc01b8698851d47994426477e0889a84
Estevan Gonzalez <estevangonzalez80@gmail.com>
2022-03-19 20:22:16 -0700

    Updated login pages

--- pages/login.js
+++ pages/login.js
@@ -1,8 +1,57 @@
-import { useState } from 'react';
+import React, {useState} from 'react';
+import { FormControl } from '@mui/material';
+import { Input } from '@mui/material';
+import { Button } from '@mui/material';
+import { Card } from '@mui/material';
+import { makeStyles } from '@mui/styles';
+import FacebookIcon from '@mui/icons-material/Facebook';
+import GoogleIcon from '@mui/icons-material/Google';
 
 import { useAuthContext } from '../utils/context/AuthProvider';
 
+const useStyles = makeStyles({
+    card: {
+      background: '#B0B4D4',
+      flexDirection: 'column',
+      border: 0,
+      borderRadius: 3,
+      boxShadow: 20,
+      color: 'white',
+      height: '100%',
+      padding: '0 30px',
+      maxWidth:'60%',
+      alignItems: 'center',
+      justifyContent: 'center'
+    },
+    googleButton: {
+      position:'relative',
+      alignItems: 'center',
+      justifyContent: 'center',
+      backgroundColor:'white',
+      variant: 'contained',
+      display: 'flex',
+      width:250
+    },
+    facebookButton: {
+      position: 'relative',
+      alignItems: 'center',
+      justifyContent: 'center',
+      backgroundColor:'#4267B2',
+      color:'white',
+      variant: 'contained',
+      display: 'flex',
+      width: 250
+    },
+    container: {
+      flexDirection:'row',
+      justifyContent: 'center',
+      alignItems:'center'
+    }
+  });
+
+
 export default function Login() {
+    const classes = useStyles();
   const { user, login, signInWithGoogle, signInWithFacebook } = useAuthContext(); // check if user context import is necessary
   const [loginInfo, setLoginInfo] = useState({
     email: '',
@@ -23,16 +72,22 @@ export default function Login() {
 
     return (
         <div>
-          <form onSubmit={(e) => handleStandardLogin(e)}>
-            <input onChange={(e) => handleLoginFormInput(e, 'email')} placeholder="Email"></input>
-            <input onChange={(e) => handleLoginFormInput(e, 'password')} placeholder="Password"></input>
-            <button type="submit">Login</button>
-          </form>
-          <div>
-            <button onClick={signInWithGoogle}>Google</button>
-            <button onClick={signInWithFacebook}>Facebook</button>
+          <Card className={classes.card}>
+          <FormControl onSubmit={(e) => handleStandardLogin(e)}>
+
+            <Input onChange={(e) => handleLoginFormInput(e, 'email')} placeholder="Email"></Input>
+            <Input onChange={(e) => handleLoginFormInput(e, 'password')} placeholder="Password"></Input>
+            <Button type="submit">Login</Button>
+          </FormControl>
+                <div className={{flexDirection:'column'}}>
+               <Button onClick={signInWithGoogle} className={classes.googleButton} startIcon={<GoogleIcon />}>Continue with Google</Button>
+               <Button onClick={signInWithFacebook} className={classes.facebookButton} startIcon={<FacebookIcon/>}>Continue with Facebook</Button>
+               </div>
               <p><a href="/">Go Home</a></p>
+          </Card>
           </div>
-        </div>
+
     )
 }
+
+
