9dfd2f20c7c315311de6220dbcbad7cff661345f
Tiffanie Vu <tiffanyv.330@gmail.com>
2022-03-21 12:21:06 -0700

    Start ProfileView

--- pages/[username]/my-profile.js
+++ pages/[username]/my-profile.js
@@ -1,3 +1,10 @@
 // Profile View
+import MyProfile from '../../components/Profiles/MyProfile.js'
 
-export default function MyProfile(props) {}
+export default function myProfile(props) {
+  return (
+    <div className="my-profile-view">
+      <MyProfile/>
+    </div>
+  )
+}
