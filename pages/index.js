9643e9ff7009fbd1311831c03058386a96d408b7
changerbang <matthewtchang25@gmail.com>
2022-03-21 19:59:24 -0700

    Handle merge conflicts w/ Auth

--- pages/index.js
+++ pages/index.js
@@ -9,25 +9,29 @@ import CreateClassModal from '../components/CreateClassModal/CreateClassModal.js
 
 export default function LandingPage() {
   return (
-        <CreateClassModal />
-  //   <div className={styles.homePage}>
-  //   <Box sx={{ flexGrow: 1 }}>
-  //   <AppBar className={styles.homeAppBar} position="static">
-  //     <Toolbar>
-  //       <Typography className={styles.siteTitle} variant="h4" component="div" sx={{ flexGrow: 1 }}>
-  //         ClassMates
-  //       </Typography>
-  //       <Button color="inherit" className={styles.loginButton}>Login</Button>
-  //       <Button color="inherit" className={styles.signupButton}>Sign Up</Button>
-  //     </Toolbar>
-  //   </AppBar>
-  // </Box>
-  //     <div className={styles.motto}>
-  //       <h1>THE LATEST IN ONLINE CLASS MANAGEMENT</h1>
-  //     </div>
-  //     <div className={styles.reporter}>
-  //       <h7>"It's so simple to use and the style is AMAZING!!!"  - Q. Maki Hack Reactor</h7>
-  //     </div>
-  //   </div>
+        // <CreateClassModal />
+        <div className={styles.homePage}>
+        <Box sx={{ flexGrow: 1 }}>
+        <AppBar className={styles.homeAppBar} position="static">
+          <Toolbar>
+            <Typography className={styles.siteTitle} variant="h4" component="div" sx={{ flexGrow: 1 }}>
+              ClassMates
+            </Typography>
+            <Link href="/login">
+              <Button color="inherit" className={styles.loginButton}>Login</Button>
+            </Link>
+            <Link href="/signup">
+              <Button color="inherit" className={styles.signupButton}>Sign Up</Button>
+            </Link>
+          </Toolbar>
+        </AppBar>
+      </Box>
+          <div className={styles.motto}>
+            <h1>THE LATEST IN ONLINE CLASS MANAGEMENT</h1>
+          </div>
+          <div className={styles.reporter}>
+            <h7>"It's so simple to use and the style is AMAZING!!!"  - Q. Maki Hack Reactor</h7>
+          </div>
+        </div>
   )
 }
