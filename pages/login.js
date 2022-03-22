4092cc6e067bfba5f41e1d6d160c6b8405d8fd9f
Chris Josephs <chrismjosephs@gmail.com>
2022-03-22 14:21:31 -0700

    switch a tag link with Link component, remove repeated props in grid component, comment out loading and error state code

--- pages/login.js
+++ pages/login.js
@@ -1,23 +1,31 @@
-import { auth } from '../utils/api/firebase.config'
-
-import { useAuthContext } from '../utils/context/AuthProvider';
 import React, { useState } from 'react';
+import Link from 'next/link';
+import { useRouter } from 'next/router';
+
 import { FormControl } from '@mui/material';
 import { Input } from '@mui/material';
 import { Button } from '@mui/material';
 import { Card, Grid } from '@mui/material';
 import { makeStyles } from '@mui/styles';
+import CircularProgress from '@mui/material/CircularProgress';
 import FacebookIcon from '@mui/icons-material/Facebook';
 import GoogleIcon from '@mui/icons-material/Google';
-import { useRouter } from 'next/router';
-import useStyles from '../utils/styles/signup.module'
 
+import { useAuthContext } from '../utils/context/AuthProvider';
+import { auth } from '../utils/api/firebase.config'
+import useStyles from '../utils/styles/signup.module'
 
 export default function Login() {
   const classes = useStyles();
   const router = useRouter();
 
-  const { user, login, signInWithGoogle, signInWithFacebook } = useAuthContext(); // check if user context import is necessary
+  const {
+     user, login,
+     loading,
+     error,
+     signInWithGoogle, signInWithFacebook
+  } = useAuthContext();
+
   const [loginInfo, setLoginInfo] = useState({
     email: '',
     password: ''
@@ -32,13 +40,39 @@ export default function Login() {
 
   const handleStandardLogin = (e) => {
     e.preventDefault();
+    setIsLoading(true);
     console.log(loginInfo.email, loginInfo.password)
     login(loginInfo.email, loginInfo.password)
+      // .then(() => setIsLoading(false));
   }
   if (user) {
     router.push('/app/my-courses');
     return null;
   }
+
+  // if (isLoading) {
+  //   return (
+  //     <CircularProgress />
+  //   )
+  // }
+
+  // if (error) {
+  //   return (
+  //     <Grid container
+  //         spacing={0}
+  //         direction="column"
+  //         alignItems="center"
+  //         justifyContent="center"
+  //         alignItems="center"
+  //         style={{ minHeight: '100vh' }}
+  //       >
+  //         <Card className={classes.card}>
+  //           <p>There was a problem logging you in. <Link href="/login"><a>Return to Log in</a></Link></p>
+  //         </Card>
+  //     </Grid>
+  //   )
+  // }
+
   return(
     <div className={classes.root}>
       <Grid container
@@ -46,7 +80,6 @@ export default function Login() {
         direction="column"
         alignItems="center"
         justifyContent="center"
-        alignItems="center"
         style={{ minHeight: '100vh' }}
       >
         <Card className={classes.card}>
@@ -59,7 +92,7 @@ export default function Login() {
             <Button sx={{my:2}} onClick={signInWithGoogle} className={classes.googleButton} startIcon={<GoogleIcon />}>Continue with Google</Button>
             <Button onClick={signInWithFacebook} className={classes.facebookButton} startIcon={<FacebookIcon />}>Continue with Facebook</Button>
           </div>
-          <p><a href="/">Go Home</a></p>
+          <Link href="/"><a>Go Home</a></Link>
         </Card>
       </Grid>
     </div>
