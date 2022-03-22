a70ba34ffead55ed55b537506b6c5edaafbd19f3
changerbang <matthewtchang25@gmail.com>
2022-03-21 17:16:18 -0700

    Set up CreateClassModal file structure

---
+++ components/CreateClassModal/CreateClassModal.js
@@ -0,0 +1,17 @@
+import {FormControl, InputLabel, Input, FormHelperText, TextField } from '@mui/material';
+
+
+
+
+
+export default CreateClassModal = () => {
+  return (
+    <div>
+      <FormControl>
+        <InputLabel>
+          <Input></Input>
+        </InputLabel>
+      </FormControl>
+    </div>
+  )
+}
