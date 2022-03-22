f809569be78a777408f3b3cd09dc09a79dd4c216
Teresa Lew <teresalew92@gmail.com>
2022-03-21 17:35:26 -0700

    completed get queries

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,29 @@
 // Landing Page
 
 import Head from 'next/head'
+import { getUserInfo } from '../db/users';
+import {
+  getCourses,
+  getCoursesById,
+  getCoursesByMentorId,
+  getCoursesByMenteeId,
+  getCoursesBySubjectName,
+  getAllSubjects,
+  createNewCourse,
+} from '../db/courses';
+
 
 export default function LandingPage() {
+  getUserInfo()
+  getCourses()
+  getCoursesById()
+  getCoursesByMentorId()
+  getCoursesByMenteeId()
+  getCoursesBySubjectName()
+  getAllSubjects()
+  createNewCourse()
+
+
   return (
     <h1>Landing Page</h1>
   )
