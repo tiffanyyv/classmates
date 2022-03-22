73e61a2ffa03fcbf835d52c9ca2e0ca3def001cc
tristen <tristen.urban@gmail.com>
2022-03-21 17:38:50 -0700

    Leaderboard Button implementation

--- components/NavLayout/SideBar.js
+++ components/NavLayout/SideBar.js
@@ -5,7 +5,6 @@ import { styled, useTheme } from '@mui/material/styles';
 import { Box, Container, Stack, Avatar } from '@mui/material';
 import { ListItemButton, ListItemIcon, ListItemText } from '@mui/material';
 import { Toolbar, List, CssBaseline, Typography, IconButton } from '@mui/material';
-
 import MenuIcon from '@mui/icons-material/Menu';
 import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
 import ChevronRightIcon from '@mui/icons-material/ChevronRight';
@@ -16,6 +15,7 @@ import SearchOutlinedIcon from '@mui/icons-material/SearchOutlined';
 
 
 import defaultProfilePic from '../../utils/constants';
+import Leaderboard from '../features/MentorRanking/MentorRanking.js';
 import styles from '../../utils/styles/NavLayoutStyles/SideBar.module.css';
 import { openedMixin, closedMixin, SideBarDrawerHeader, SideBarAppBar, SideBarDrawer } from '../../components/basecomponents/SideBarStyles.js';
 
@@ -69,6 +69,7 @@ export default function SideBar({ children, ...props }) {
             ClassMates
           </Typography>
           <Container className={styles.profileIcon} sx={{ flexGrow: 0 }}>
+            <Leaderboard />
             <Typography className={styles.profileName}>Current User</Typography>
             <Avatar
               className={styles.profileAvatar}
