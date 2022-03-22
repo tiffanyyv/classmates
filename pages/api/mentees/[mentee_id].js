576ac3e2195e469b891e53298641e830821b01f3
Teresa Lew <teresalew92@gmail.com>
2022-03-22 00:33:35 -0700

    Implemented POST endpoint for /api/mentees/index.js

--- pages/api/mentees/[mentee_id].js
+++ pages/api/mentees/[mentee_id].js
@@ -1,6 +1,16 @@
 // GET: /pages/api/mentees/[mentee_id].js
+import { db } from '../../../utils/api/firebase.config';
+import { collection, doc, getDoc } from "firebase/firestore";
 
-export default function getMenteeInfo(req, res) {
+export default async function getMenteeInfo(req, res) {
+  // NEED UID FROM AUTH OBJ
   const { mentee_id } = req.query;
-  // open to add query
+
+  try {
+    const docRef = doc(db, 'mentees', mentee_id);
+    const docSnap = await getDoc(docRef);
+    res.status(200).json(docSnap.data());
+  } catch (err) {
+    res.status(400).send(`Error adding new mentee: ${err}`);
+  }
 }
--- pages/api/mentees/index.js
+++ pages/api/mentees/index.js
@@ -1,5 +1,39 @@
 // POST: /pages/mentees/index.js
+import { db } from '../../../utils/api/firebase.config';
+import { collection, doc, getDoc, addDoc } from "firebase/firestore";
+import defaultProfilePic from '../../../utils/constants/index';
 
-export default function addMentee(req, res) {
-  // open to add query
+export default async function addMentee(req, res) {
+  const {
+    account_type,
+    firstName,
+    lastName,
+    location,
+    uid,
+    username
+  } = req.body;
+
+  try {
+    const docRef = await addDoc(collection(db, 'courses'), {
+      courses: [],
+      username,
+      location,
+      name: {
+        'first_name': firstName,
+        'last_name': lastName
+      },
+      id: uid,
+      photo: defaultProfilePic,
+      account_type,
+      endorsements: 0,
+      description: ''
+    });
+
+    res.status(200).json(`Successfully posted course`);
+
+  } catch (err) {
+
+    res.status(400).send(`Error posting new course: ${err}`);
+
+  }
 }
