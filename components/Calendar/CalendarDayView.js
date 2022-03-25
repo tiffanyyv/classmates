0f7339f42ad97de24675a092da525f5ed38ef948
changerbang <matthewtchang25@gmail.com>
2022-03-24 21:24:07 -0700

    Implement SSR and dynamic window sizing

--- components/Calendar/CalendarDayView.js
+++ components/Calendar/CalendarDayView.js
@@ -1,42 +1,22 @@
-// Calendar Widget //
 import Head from 'next/head';
-import dynamic from 'next/dynamic';
 import { useState, useEffect } from 'react';
 import { styled } from '@mui/material/styles';
 import Paper from '@mui/material/Paper';
 import { ViewState } from '@devexpress/dx-react-scheduler';
-import {  Scheduler,
-  WeekView,
-  Toolbar,
-  DateNavigator,
-  Appointments,
-  TodayButton,
-  AppointmentTooltip,
-} from '@devexpress/dx-react-scheduler-material-ui';
+import { Scheduler, DayView, Toolbar, DateNavigator, Appointments, TodayButton, AppointmentTooltip } from '@devexpress/dx-react-scheduler-material-ui';
 
-import TooltipContent from '../../components/Calendar/subcomponents/TooltipContent.js';
+import TooltipContent from '../Calendar/subcomponents/TooltipContent.js';
 import styles from '../../utils/styles/CalendarStyles/Calendar.module.css';
-import { TimeTableCell, DayScaleCell } from '../../components/Calendar/Calendar.js';
-import CreateClassModal from '../../components/CreateClassModal/CreateClassModal.js';
-import CalendarDayView from '../../components/Calendar/CalendarDayView.js';
+import { TimeTableCell, DayScaleCell } from './Calendar.js';
+import CreateClassModal from '../CreateClassModal/CreateClassModal.js';
 
-import {
-  getCoursesByMentorId,
-  getCoursesByMenteeId,
-  getUserInfo,
-} from '../../utils/api/apiCalls.js';
-import useWindowWidth from '../../utils/customHooks/useWindowWidth.js'
+import { getCoursesByMentorId, getCoursesByMenteeId, getUserInfo } from '../../utils/api/apiCalls.js';
 
 
 export default function Calendar({ userInfo, formattedAllCoursesData }) {
-
   const [appointmentData, setAppointmentData] = useState(formattedAllCoursesData);
   const [userType, setUserType] = useState(userInfo.account_type);
   const [currUserId, setCurrUserId] = useState(userInfo.id);
-
-  if (typeof window === 'object') {
-    const { width } = useWindowWidth();
-  }
 
   const getCoursesData = () => {
     if (userType === 'Mentor') {
@@ -89,11 +69,6 @@ export default function Calendar({ userInfo, formattedAllCoursesData }) {
     }
   }
 
-  if(typeof width === 'number') {
-    if (width <= 750) {
-      return <CalendarDayView userInfo={userInfo} formattedAllCoursesData={formattedAllCoursesData} />
-    }
-  }
   return (
     <div className="pageData">
       <div className={styles.calendarContainer}>
@@ -107,13 +82,11 @@ export default function Calendar({ userInfo, formattedAllCoursesData }) {
               height={'950'}
             >
               <ViewState
-              defaultCurrentViewName="Week"
+              defaultCurrentViewName="Day"
               />
-              <WeekView
+              <DayView
                 startDayHour={6}
                 endDayHour={22}
-                timeTableCellComponent={TimeTableCell}
-                dayScaleCellComponent={DayScaleCell}
               />
               <Toolbar />
               <DateNavigator />
@@ -135,36 +108,4 @@ export default function Calendar({ userInfo, formattedAllCoursesData }) {
       </div>
     </div>
   )
-}
-
-export async function getServerSideProps(context) {
-  const userId = context.params['user-id'];
-  const userInfo = await getUserInfo(userId);
-  let allCoursesData;
-
-  if (userInfo.account_type === 'Mentor') {
-    allCoursesData = await getCoursesByMentorId(userInfo.id)
-  } else if (userInfo.account_type === 'Mentee') {
-    allCoursesData = await getCoursesByMenteeId(userInfo.id)
-  }
-
-  let formattedAllCoursesData = allCoursesData.map((course) => {
-    return {
-      title: course.name,
-      startDate: course.start_date,
-      endDate: course.end_date,
-      zoomLink: course.meeting_url,
-      capacity: course.capacity,
-      description: course.description,
-      id: course.id,
-      subject: course.subject,
-      photo: course.photo,
-      mentor: course.mentor,
-      mentees: course.mentees,
-    }
-  });
-
-  return {
-    props: { userInfo, formattedAllCoursesData }
-  }
 }
