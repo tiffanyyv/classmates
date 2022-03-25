b50760a90e29ae04b58657fd0b0822443fec9677
Chris Josephs <chrismjosephs@gmail.com>
2022-03-25 10:55:59 -0700

    remove unecessary comments

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,7 @@
-// import global CSS
 import '../utils/styles/globals.css'
 
-// import contexts
 import { AuthProvider } from '../utils/context/AuthProvider';
 
-// import layout components
 import NavLayout from '../components/NavLayout/NavLayout';
 
 
